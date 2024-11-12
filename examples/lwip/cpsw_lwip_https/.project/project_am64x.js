095892d5934cb091223447ad12de8104de6cc99f
Abinash Ray <a-ray2@ti.com>
2024-11-12 14:29:03 +0530

    am64x: enet: Add cpsw_lwip_https example
         -update .js for a53 core
         support
    
    Fixes: SITSW-5757
    
    Signed-off-by: Abinash Ray <a-ray2@ti.com>

--- examples/lwip/cpsw_lwip_https/.project/project_am64x.js
+++ examples/lwip/cpsw_lwip_https/.project/project_am64x.js
@@ -64,6 +64,34 @@ const includes_freertos_r5f = {
     ],
 };
 
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am64x/a53",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/utils/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/utils/V3",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include/core",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include/phy",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/soc/k3/am64x_am243x",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/hw_include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/hw_include/mdio/V4",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-stack/src/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-port/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-port/freertos/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/lwipif/inc",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-stack/contrib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-stack/src/apps/altcp_tls",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-config/am64x",
+        "${MCU_PLUS_SDK_PATH}/source/networking/mbedtls_library/mbedtls/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/mbedtls_library/mbedtls_ti",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/dpl/"
+    ],
+};
+
 const libs_freertos_r5f = {
     common: [
         "freertos.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
@@ -74,6 +102,19 @@ const libs_freertos_r5f = {
         "lwip-contrib-freertos.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
         "board.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
         "mbedtls.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "board.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "enet-cpsw.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "lwipif-cpsw-freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "lwip-freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "lwip-contrib-freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "mbedtls.am64x.a53.gcc-aarch64.${ConfigName}.lib",
     ],
 };
 
@@ -89,10 +130,26 @@ const defines_r5f = {
     ],
 };
 
+
+const defines_a53 = {
+    common: [
+        "MBEDTLS_CONFIG_FILE=\\\"alt_config.h\\\"",
+    ],
+};
+
 const cflags_r5f = {
     release: [
         "-Oz",
         "-flto"
+    ],
+};
+
+const cflags_a53 = {
+    common: [
+        "-Wno-unused-function",
+    ],
+    release: [
+        "-flto",
     ],
 };
 
@@ -101,6 +158,16 @@ const lflags_r5f = {
         "--zero_init=on",
         "--use_memset=fast",
         "--use_memcpy=fast"
+    ],
+};
+
+const lflags_a53 = {
+    common: [
+    ],
+};
+const loptflags_a53 = {
+    release: [
+        "-flto"
     ],
 };
 
@@ -125,8 +192,22 @@ const templates_freertos_r5f =
     },
 ];
 
+const templates_freertos_a53 =
+[
+    
+    {
+        input: ".project/templates/am64x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "appMain",
+            stackSize : "8192",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-evm", os: "freertos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64", board: "am64x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -173,6 +254,31 @@ function getComponentBuildProperty(buildOption) {
             build_property.lflags = lflags_r5f;
             build_property.defines = defines_r5f;
             build_property.projectspecLnkPath = linker_includePath_freertos;
+        }
+    }
+
+    if(buildOption.cpu.match(/a53*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            const _ = require('lodash');
+            let libdirs_freertos_cpy = _.cloneDeep(libdirs_freertos);
+            /* Logic to remove generated/ from libdirs_freertos, it generates warning for ccs build */
+            if (buildOption.isProjectSpecBuild === true)
+            {
+                var delIndex = libdirs_freertos_cpy.common.indexOf('generated');
+                if (delIndex !== -1) {
+                    libdirs_freertos_cpy.common.splice(delIndex, 1);
+                }
+            }
+            build_property.includes = includes_freertos_a53;
+            build_property.libdirs = libdirs_freertos_cpy;
+            build_property.libs = libs_freertos_a53;
+            build_property.templates = templates_freertos_a53;
+            build_property.defines = defines_a53;
+            build_property.cflags = cflags_a53;
+            build_property.lflags = lflags_a53;
+            build_property.projectspecLnkPath = linker_includePath_freertos;
+            build_property.loptflags = loptflags_a53;
         }
     }
 
