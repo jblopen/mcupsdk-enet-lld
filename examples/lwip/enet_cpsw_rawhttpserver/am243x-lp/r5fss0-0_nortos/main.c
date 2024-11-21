10421d79edfb53c41eb9adf3f3f0575c092efbda
Pradeep HN <p-hn@ti.com>
2024-11-21 08:00:58 +0530

    allsocs: enet: Update main.c from Autogen
    - main.c template is changed to keep the board open and drvier open
    outside user application code as its is a common code.
    
    Fixes: MCUSDK-13757
    Signed-off-by: Pradeep HN <p-hn@ti.com>

--- examples/lwip/enet_cpsw_rawhttpserver/am243x-lp/r5fss0-0_nortos/main.c
+++ examples/lwip/enet_cpsw_rawhttpserver/am243x-lp/r5fss0-0_nortos/main.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018-2021 Texas Instruments Incorporated
+ *  Copyright (C) 2023-2024 Texas Instruments Incorporated
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -33,15 +33,30 @@
 #include <stdlib.h>
 #include "ti_drivers_config.h"
 #include "ti_board_config.h"
+#include "ti_drivers_open_close.h"
+#include "ti_board_open_close.h"
 
 void appMain(void *args);
 
-int main(void)
+int main()
 {
     System_init();
     Board_init();
 
+    int32_t status = SystemP_SUCCESS;
+
+    /* Open drivers */
+    Drivers_open();
+    /* Open flash and board drivers */
+    status = Board_driversOpen();
+    DebugP_assert(status==SystemP_SUCCESS);
+
     appMain(NULL);
+
+    /* Close board and flash drivers */
+    Board_driversClose();
+    /* Close drivers */
+    Drivers_close();
 
     Board_deinit();
     System_deinit();
