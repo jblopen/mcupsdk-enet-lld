a04362860f10f7e4e38f1008ce344208a15e4d2e
Nitika Verma <nitika.verma@ti.com>
2024-11-21 12:33:00 +0530

    allsocs: test: Add ethphy module to test folder examples
    - Add ethphy module to example.syscfg
    - Add ethphy path to makefiles, .project and .projectspec files
    - Update ethphy path in enet_custom_board_config files
    
    Fixes: MCUREQ-3754
    
    Signed-off-by: Nitika Verma <nitika.verma@ti.com>

--- test/enet_layer2_icssg/icssg_layer2_switch/am64x-evm/r5fss0-0_freertos/enet_custom_board_config.c
+++ test/enet_layer2_icssg/icssg_layer2_switch/am64x-evm/r5fss0-0_freertos/enet_custom_board_config.c
@@ -35,8 +35,8 @@
 #include <stdint.h>
 #include <enet.h>
 #include <networking/enet/core/include/phy/enetphy.h>
-#include <networking/enet/core/include/phy/dp83867.h>
-#include <networking/enet/core/include/phy/dp83869.h>
+#include <dp83867.h>
+#include <dp83869.h>
 
 #include <enet_apputils.h>
 #include <kernel/dpl/SystemP.h>
@@ -46,11 +46,11 @@
 
 #if (ENETBOARD_SYSCFG_CUSTOM_BOARD == 1)
 /* PHY drivers */
-extern EnetPhy_Drv gEnetPhyDrvGeneric;
-extern EnetPhy_Drv gEnetPhyDrvDp83869;
+extern Phy_DrvObj_t gEnetPhyDrvGeneric;
+extern Phy_DrvObj_t gEnetPhyDrvDp83869;
 
 /*! \brief All the registered PHY specific drivers. */
-static const EnetPhyDrv_Handle gEnetPhyDrvs[] =
+static const EthPhyDrv_If gEnetPhyDrvs[] =
 {
     &gEnetPhyDrvDp83869,   /* DP83869 */
     &gEnetPhyDrvGeneric,   /* Generic PHY - must be last */
